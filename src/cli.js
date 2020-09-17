42d34e372a37621f7f9f1691aa1a14bbc37393f7
harshraj153 <47703818+harshraj153@users.noreply.github.com>
2020-09-17 06:32:14 +0400

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -15,7 +15,7 @@ const struct = {
 
 const commands = {};
 let systemData = {};
-const rootPath = 'users/codebytere/root';
+const rootPath = 'users/harshitraj/root';
 
 const getDirectory = () => localStorage.directory;
 const setDirectory = (dir) => {
